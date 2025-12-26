83459d9ce4b4fa55ec3b18898980f3e87851cb60
Nicolas Hochart <76011128+nicochart@users.noreply.github.com>
2025-12-26 15:01:44 +0100

    [Mod Version] Backported to Fabric 1.20.1

--- src/main/java/fr/factionbedrock/mixin/PlayerTickMixin.java
+++ src/main/java/fr/factionbedrock/mixin/PlayerTickMixin.java
@@ -17,8 +17,8 @@ public class PlayerTickMixin
 
 		if (self.isSpectator())
 		{
-			if (self.noClip && !self.hasStatusEffect(ASNStatusEffects.ALLOW_NOCLIP)) {self.noClip = false;}
-			else if (!self.noClip && self.hasStatusEffect(ASNStatusEffects.ALLOW_NOCLIP)) {self.noClip = true;}
+			if (self.noClip && !self.hasStatusEffect(ASNStatusEffects.ALLOW_NOCLIP.value())) {self.noClip = false;}
+			else if (!self.noClip && self.hasStatusEffect(ASNStatusEffects.ALLOW_NOCLIP.value())) {self.noClip = true;}
 		}
 	}
 }
